a8f4e17d1e4234281b72fb332cef54d8375a474d
mjperales <email@email.com>
2025-07-05 13:26:30 -0500

    chore: clean up code

--- helpers/reverseString.test.ts
+++ helpers/reverseString.test.ts
@@ -4,7 +4,6 @@ describe("reverseString()", () => {
   test('should equal ["o","l","l","e","h"]', () => {
     let test1 = ["h", "e", "l", "l", "o"];
     reverseString(test1);
-    console.log(test1);
     expect(test1).toEqual(["o", "l", "l", "e", "h"]);
   });
   test('should equal ["h","a","n","n","a","H"]', () => {
